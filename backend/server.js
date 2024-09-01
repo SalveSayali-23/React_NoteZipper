eb7026be84197d33d0487cdbc962a2f34a28af58
Sayali <salvesayali23@gmail.com>
2024-09-02 01:22:19 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -11,6 +12,14 @@ dotenv.config();
 // console.log("JWT_SECRET:", process.env.JWT_SECRET);
 
 connectDB();
+// Configure CORS
+app.use(
+  cors({
+    origin: "https://react-notezipper-frontend.onrender.com", // Update this with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Add the methods your API supports
+    // credentials: true, // If you use cookies for authentication
+  })
+);
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running..");
